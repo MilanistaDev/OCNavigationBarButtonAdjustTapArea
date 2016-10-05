4600187d7d8180c2dcf1d2675bc942d5ef2109ea
Takuya ASO <milanista224@me.com>
2016-10-06 00:30:52 +0900

    [OC]Add UINavigationBar SubClass

---
+++ OCNavigationBarButtonAdjustTapArea/MainNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  MainNavigationBar.h
+//  OCNavigationBarButtonAdjustTapArea
+//
+//  Created by 麻生 拓弥 on 2016/10/06.
+//  Copyright © 2016年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainNavigationBar : UINavigationBar
+
+@end
---
+++ OCNavigationBarButtonAdjustTapArea/MainNavigationBar.m
@@ -0,0 +1,21 @@
+//
+//  MainNavigationBar.m
+//  OCNavigationBarButtonAdjustTapArea
+//
+//  Created by 麻生 拓弥 on 2016/10/06.
+//  Copyright © 2016年 麻生 拓弥. All rights reserved.
+//
+
+#import "MainNavigationBar.h"
+
+@implementation MainNavigationBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
