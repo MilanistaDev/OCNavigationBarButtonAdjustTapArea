19de83427fb24cad8e0ac94ade1ae42fa6959183
Takuya ASO <milanista224@me.com>
2016-10-07 22:56:56 +0900

    [OC]Add New VC class and apply

---
+++ OCNavigationBarButtonAdjustTapArea/AdjustTapAreaViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdjustTapAreaViewController.h
+//  OCNavigationBarButtonAdjustTapArea
+//
+//  Created by 麻生 拓弥 on 2016/10/07.
+//  Copyright © 2016年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdjustTapAreaViewController : UIViewController
+
+@end
